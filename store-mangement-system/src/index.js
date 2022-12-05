0eb75e74f16f5d59f6e82c816c063a80a4e69581
unknown <sarveshbhosale111@gmail.com>
2022-12-05 22:12:21 +0000

    Template Added

--- store-mangement-system/src/index.js
+++ store-mangement-system/src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import AppWrapper from './AppWrapper';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = configureStore({reducer: {}}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-         <App /> 
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <HashRouter>
+            <AppWrapper></AppWrapper>
+        </HashRouter>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
