cab47edc768d02693e59ac9152cb5388faf88458
Rahulsogam <sogam.rahul188@gmail.com>
2022-12-14 04:39:05 +0000

    Category getAPI

--- StoreManagementBackend/src/main/java/com/stms/config/SqlQuery.java
+++ StoreManagementBackend/src/main/java/com/stms/config/SqlQuery.java
@@ -4,4 +4,7 @@ public class SqlQuery {
 
 	public static final String GET_ALL_EMP="select * from Employee where employee_id='101'";
 	public static final String GET_ALL_EMPSal="select e.employee_id,d.Designation,d.designation_salary from Employee e, designation d where e.Designation='employee'";
+
+	public static final String GET_ALL_CAT="select * from Category";
+
 }
--- StoreManagementBackend/src/main/java/com/stms/controller/EmpController.java
+++ StoreManagementBackend/src/main/java/com/stms/controller/EmpController.java
@@ -1,19 +1,14 @@
 package com.stms.controller;
 
-import java.util.List;
-
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stms.config.Response;
-import com.stms.model.Employee;
 import com.stms.service.Empservice;
 
 @RestController
@@ -23,21 +18,6 @@ public class EmpController {
 	@Autowired
 	Empservice empservice;
 	
-	/*
-	 * @RequestMapping(path = "api/v1", method = RequestMethod.GET) public
-	 * ResponseEntity<Response> getAllStudents() throws
-	 * StudentManagementSystemException {
-	 * 
-	 * List<Student> studentList = studentService.getAllStudents();
-	 * 
-	 * if (studentList == null || studentList.isEmpty()) {
-	 * 
-	 * logger.info("No data found or list is empty"); throw new
-	 * StudentManagementSystemException("", HttpStatus.OK, studentList); }
-	 * 
-	 * return new ResponseEntity<Response>(new Response("success", studentList,
-	 * null), HttpStatus.OK); }
-	 */
 	@GetMapping(path = "/All")
 	ResponseEntity<Response> getAll(){
 		System.out.println("COntroller"+" "+empservice.getAllEmp());
